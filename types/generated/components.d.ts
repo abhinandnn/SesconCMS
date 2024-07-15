ccd9a8cc015f24694533131380d01c7a1af88747
Abhinandan <abhinandnn21@gmail.com>
2024-07-15 18:58:52 +0530

    added home page

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,46 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface CardProjectCard extends Schema.Component {
+  collectionName: 'components_card_project_cards';
+  info: {
+    displayName: 'project_card';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    image: Attribute.Media;
+  };
+}
+
+export interface QnAQnA extends Schema.Component {
+  collectionName: 'components_qn_a_qn_as';
+  info: {
+    displayName: 'QnA';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    content: Attribute.Text;
+  };
+}
+
+export interface TextAboutusText extends Schema.Component {
+  collectionName: 'components_text_aboutus_texts';
+  info: {
+    displayName: 'aboutus_text';
+  };
+  attributes: {
+    para1: Attribute.Text;
+    para2: Attribute.Text;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'card.project-card': CardProjectCard;
+      'qn-a.qn-a': QnAQnA;
+      'text.aboutus-text': TextAboutusText;
+    }
+  }
 }
--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,33 +362,36 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
-export interface ApiLandingPageLandingPage extends Schema.CollectionType {
-  collectionName: 'landing_pages';
+export interface ApiHomePageHomePage extends Schema.SingleType {
+  collectionName: 'home_pages';
   info: {
-    singularName: 'landing-page';
-    pluralName: 'landing-pages';
-    displayName: 'Landing Page';
+    singularName: 'home-page';
+    pluralName: 'home-pages';
+    displayName: 'home-page';
     description: '';
   };
   options: {
     draftAndPublish: true;
   };
   attributes: {
-    Header: Attribute.String;
-    Header2: Attribute.String;
-    something: Attribute.Media;
-    about: Attribute.Text;
+    hero_section_imgs: Attribute.Media;
+    aboutus_carousel_imgs: Attribute.Media;
+    aboutus_text: Attribute.Text;
+    project_card: Attribute.Component<'card.project-card', true>;
+    our_availability_text: Attribute.Component<'text.aboutus-text'>;
+    our_client_imgs: Attribute.Media;
+    QnA: Attribute.Component<'qn-a.qn-a', true>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
-      'api::landing-page.landing-page',
+      'api::home-page.home-page',
       'oneToOne',
       'admin::user'
     > &
       Attribute.Private;
     updatedBy: Attribute.Relation<
-      'api::landing-page.landing-page',
+      'api::home-page.home-page',
       'oneToOne',
       'admin::user'
     > &
@@ -624,6 +627,53 @@ export interface PluginContentReleasesReleaseAction
   };
 }
 
+export interface PluginI18NLocale extends Schema.CollectionType {
+  collectionName: 'i18n_locale';
+  info: {
+    singularName: 'locale';
+    pluralName: 'locales';
+    collectionName: 'locales';
+    displayName: 'Locale';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  pluginOptions: {
+    'content-manager': {
+      visible: false;
+    };
+    'content-type-builder': {
+      visible: false;
+    };
+  };
+  attributes: {
+    name: Attribute.String &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+          max: 50;
+        },
+        number
+      >;
+    code: Attribute.String & Attribute.Unique;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'plugin::i18n.locale',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUsersPermissionsPermission
   extends Schema.CollectionType {
   collectionName: 'up_permissions';
@@ -775,53 +825,6 @@ export interface PluginUsersPermissionsUser extends Schema.CollectionType {
   };
 }
 
-export interface PluginI18NLocale extends Schema.CollectionType {
-  collectionName: 'i18n_locale';
-  info: {
-    singularName: 'locale';
-    pluralName: 'locales';
-    collectionName: 'locales';
-    displayName: 'Locale';
-    description: '';
-  };
-  options: {
-    draftAndPublish: false;
-  };
-  pluginOptions: {
-    'content-manager': {
-      visible: false;
-    };
-    'content-type-builder': {
-      visible: false;
-    };
-  };
-  attributes: {
-    name: Attribute.String &
-      Attribute.SetMinMax<
-        {
-          min: 1;
-          max: 50;
-        },
-        number
-      >;
-    code: Attribute.String & Attribute.Unique;
-    createdAt: Attribute.DateTime;
-    updatedAt: Attribute.DateTime;
-    createdBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-    updatedBy: Attribute.Relation<
-      'plugin::i18n.locale',
-      'oneToOne',
-      'admin::user'
-    > &
-      Attribute.Private;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -832,15 +835,15 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
-      'api::landing-page.landing-page': ApiLandingPageLandingPage;
+      'api::home-page.home-page': ApiHomePageHomePage;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
+      'plugin::i18n.locale': PluginI18NLocale;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
-      'plugin::i18n.locale': PluginI18NLocale;
     }
   }
 }
