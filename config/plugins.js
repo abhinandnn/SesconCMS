bf09564769d9789b22542e7d70c9ff02d6d3797e
Abhinandan <abhinandnn21@gmail.com>
2024-09-12 06:53:18 +0530

    fixed minor issues

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,7 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'c8e4b66b208d2d9bcd5751bf7c40fc0047cfca4cfea10fc307f04a215bc55b17'),
+      },
+    },
+  });
