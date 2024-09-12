dc36e4ac6c1d6608a53749bcbc545015bd506e8b
Abhinandan <abhinandnn21@gmail.com>
2024-09-12 09:55:49 +0530

    added jwtsecret key

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
     'users-permissions': {
       config: {
-        jwtSecret: env('JWT_SECRET', 'c8e4b66b208d2d9bcd5751bf7c40fc0047cfca4cfea10fc307f04a215bc55b17'),
+        jwtSecret: env('JWT_SECRET'),
       },
     },
   });
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS',['41ee240b64c552997fd39921b28754a14c3630d956be9679cb914043acc012c9','7487c24c5682d146a3b3b26be7dcb6c45df82d7830b38865f50ab0272876345f']),
+    keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
